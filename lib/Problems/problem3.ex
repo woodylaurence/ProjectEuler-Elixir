5bb39e3030655359d3868e069d70aad0bc848355
Laurence Woodward <laurence.woodward@highspeedtraining.co.uk>
2023-10-20 22:38:01 +0100

    Refactored problem 3 solution to use Enum methods

--- lib/Problems/problem3.ex
+++ lib/Problems/problem3.ex
@@ -1,21 +1,7 @@
 defmodule Problems.Problem3 do
   def solve do
-    factors = Math.get_factors(600_851_475_143)
-    get_largest_prime_factor(factors)
+    Math.get_factors(600_851_475_143)
+      |> Enum.reverse
+      |> Enum.find(fn x -> Math.prime?(x) end)
   end
-
-  def get_largest_prime_factor(factors, current_largest_prime_factor \\ 1)
-
-  def get_largest_prime_factor([], current_largest_prime_factor) do
-    current_largest_prime_factor
-  end
-
-  def get_largest_prime_factor([factor | rest], current_largest_prime_factor) do
-    cond do
-      Math.prime?(factor) -> get_largest_prime_factor(rest, factor)
-      true -> get_largest_prime_factor(rest, current_largest_prime_factor)
-    end
-  end
-
-
 end
