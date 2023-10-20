a5df4a4f7dc8e6584389249e6a18da5ec559bf85
Laurence Woodward <laurence.woodward@highspeedtraining.co.uk>
2023-10-20 22:34:38 +0100

    Refactored few math methods into Math module

--- lib/Utilities/Math.ex
+++ lib/Utilities/Math.ex
@@ -7,6 +7,23 @@ defmodule Math do
     rem(y, x) == 0
   end
 
+  def get_factors(x, current_factor \\ 1, factors \\ [])
+  def get_factors(x, current_factor, factors) do
+    cond do
+      current_factor > :math.sqrt(x) |> ceil -> factors |> Enum.sort
+      current_factor == :math.sqrt(x) -> factors ++ [current_factor] |> Enum.sort
+      Math.factor_of?(current_factor, x) -> get_factors(x, current_factor + 1, factors ++ [current_factor, (x / current_factor) |> trunc])
+      true -> get_factors(x, current_factor + 1, factors)
+    end
+  end
+
+  def prime?(x) do
+    case get_factors(x) do
+      [1, ^x] -> true
+      _ -> false
+    end
+  end
+
   def fibonacci() do
     Stream.unfold({0, 1}, fn {x, y} -> {x, {y, x + y}} end)
   end
