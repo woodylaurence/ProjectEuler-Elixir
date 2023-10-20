b75ce479f47aa7effd4ce58d4f8587507253a05a
Laurence Woodward <laurence.woodward@highspeedtraining.co.uk>
2023-10-20 21:29:24 +0100

    Refactored problem 2 to use infinite fibonacci sequence generator

--- lib/Problems/problem2.ex
+++ lib/Problems/problem2.ex
@@ -1,35 +1,10 @@
+require Integer
+
 defmodule Problems.Problem2 do
-  require Integer
   def solve do
-    fibonacci_terms_under_4_million = get_fibonacci_sequence_up_to(4_000_000)
-    sum_even_valued_elements(fibonacci_terms_under_4_million)
-  end
-
-  def get_fibonacci_sequence_up_to(_x, array \\ [])
-
-  def get_fibonacci_sequence_up_to(x, []) do
-    get_fibonacci_sequence_up_to(x, [0, 1])
-  end
-
-  def get_fibonacci_sequence_up_to(x, list) do
-    [n_2, n_1] = Enum.take(list, -2)
-    case n_2 + n_1 do
-      n when n <= x -> get_fibonacci_sequence_up_to(x, (list ++ [n]))
-      _ -> list
-    end
-  end
-
-  def sum_even_valued_elements(_array, accumulator \\ 0)
-
-  def sum_even_valued_elements([], accumulator) do
-    accumulator
-  end
-
-  def sum_even_valued_elements([head | tail], accumulator) when Integer.is_even(head) do
-    sum_even_valued_elements(tail, head + accumulator)
-  end
-
-  def sum_even_valued_elements([_ | tail], accumulator) do
-    sum_even_valued_elements(tail, accumulator)
+    Math.fibonacci
+      |> Enum.take_while(fn x -> x < 4_000_000 end)
+      |> Enum.filter(fn x -> Integer.is_even(x) end)
+      |> Enum.sum()
   end
 end
--- lib/Utilities/Math.ex
+++ lib/Utilities/Math.ex
@@ -1,5 +1,9 @@
 defmodule Math do
-  def multiple_of?(x, y) do
+  def multiple_of?(x, y) when y > 0 do
     rem(x, y) == 0
+  end
+
+  def fibonacci() do
+    Stream.unfold({0, 1}, fn {x, y} -> {x, {y, x + y}} end)
   end
 end
